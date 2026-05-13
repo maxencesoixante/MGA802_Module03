d0090c0e835f3e8109a0938e93f0d232dc6ed498
DuboisMax <89984245+DuboisMax@users.noreply.github.com>
2026-05-13 15:17:17 -0400

    First commit : main V1 with addition and multiplication @Maxence

---
+++ main.py
@@ -0,0 +1,28 @@
+'''On définit le global direct dans la fonction pas
+besoin de la redéfinir dans les autres fonctions'''
+
+def procedure(a,b):
+    global total
+    total = a+b
+
+def multiplication(x):
+    result = x*total
+    return result
+
+procedure(2,2)
+print(multiplication(2))
+
+def factorial(n):
+    total = 1
+    facto = 1
+    while facto<n:
+        facto+=1
+        total*=facto
+    return total
+
+print(factorial(2))
+
+print(factorial(4))
+
+
+
